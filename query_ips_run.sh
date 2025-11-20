a4e99c02fd03f39b8486c3e050a3551f0ccf4fa9
siven <no>
2025-11-20 01:50:16 +0000

    v2025_11_20

--- query_ips_run.sh
+++ query_ips_run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+export PYTHONPATH=$PWD
+cd $PYTHONPATH
+
 .venv/bin/python src/main.py
 
 # 检查上一条命令是否成功
 if [ $? -eq 0 ]; then
-    git add .
-    commit_msg="v$(date +%Y_%m_%d)"
-    git commit -m "$commit_msg"
-    git push -u origin main
+  git add .
+  commit_msg="v$(date +%Y_%m_%d)"
+  git commit -m "$commit_msg"
+  git push -u origin main
 else
-    echo "Python 程序执行失败，取消 Git push"
-    exit 1
+  echo "Python 程序执行失败，取消 Git push"
+  exit 1
 fi
+
