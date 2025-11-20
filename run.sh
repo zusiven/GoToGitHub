a02a94b0206e64480ed29f8c69b296c52cc523a9
wzk <no>
2025-11-20 09:14:31 +0800

    v2025-11-20

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@
 
 # 检查上一条命令是否成功
 if [ $? -eq 0 ]; then
+    git add .
+    commit_msg="v$(date +%Y_%m_%d)"
+    git commit -m "$commit_msg"
     git push -u origin main
 else
     echo "Python 程序执行失败，取消 Git push"
