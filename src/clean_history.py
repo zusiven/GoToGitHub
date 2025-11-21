761689b34b0f8d812f45ed565a704f8cb32834bc
may <no>
2025-11-21 11:02:12 +0800

    update

--- src/clean_history.py
+++ src/clean_history.py
@@ -5,10 +5,10 @@ from pathlib import Path
 def clean_history_data():
     save_dir = "data"
     current_time = dt.datetime.now()
-    history_time = current_time - dt.timedelta(hours=7)
+    history_time = current_time - dt.timedelta(days=7)
     # 防止程序运行失败，遗漏删除
     for i in range(299):
-        clean_time_str = (history_time - dt.timedelta(hours=i)).strftime("%Y_%m_%d")
+        clean_time_str = (history_time - dt.timedelta(days=i)).strftime("%Y_%m_%d")
         clean_path = Path(save_dir, f"{clean_time_str}.txt")
         if clean_path.exists():
             clean_path.unlink()
